44c60f34b046c7cb9379a64505cc6ee1d4cd26a6
Lauren Colls <lauren.colls@vizexplorer.com>
2018-03-16 08:49:03 +1300

    initial

---
+++ content.js
@@ -0,0 +1,23 @@
+/* Listen for messages */
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+    /* If the received message has the expected format... */
+    if (msg.text && (msg.text == "expand_all")) {
+        /* Call the specified callback, passing 
+           the web-pages DOM content as argument */
+		var endpoints = document.getElementsByClassName("endpoints")
+	    for (var i = endpoints.length - 1; i >= 0; i--) {
+	      endpoints[i].setAttribute("style", "display: block;");
+	    }
+
+    	sendResponse(true);
+    } else if (msg.text && (msg.text == "collapse_all")) {
+        /* Call the specified callback, passing 
+           the web-pages DOM content as argument */
+        var endpoints = document.getElementsByClassName("endpoints")
+        for (var i = endpoints.length - 1; i >= 0; i--) {
+          endpoints[i].setAttribute("style", "display: none;");
+        }
+
+        sendResponse(true);
+    }
+});
---
+++ popup.js
@@ -0,0 +1,22 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+  var expandAll = document.getElementById('expand-all');
+  var collapseAll = document.getElementById('collapse-all');
+
+	expandAll.addEventListener('click', function() {
+		chrome.tabs.getSelected(null, function(tab) {
+		  chrome.tabs.sendMessage(tab.id, { text: "expand_all" }, doStuffWithDOMCB);
+		});
+	}, false);
+
+
+	collapseAll.addEventListener('click', function() {	
+		chrome.tabs.getSelected(null, function(tab) {
+		  chrome.tabs.sendMessage(tab.id, { text: "collapse_all" }, doStuffWithDOMCB);
+		});
+	}, false);
+}, false);
+
+function doStuffWithDOMCB(done){
+  console.log('Expanded endpoints!')
+}
